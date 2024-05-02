1522509941a8294c18d8d8f88968e1d182613d0f
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-05-02 23:02:25 +0100

    Fixes setup_hotspot.sh script

--- .local/bin/setup_hotspot.sh
+++ .local/bin/setup_hotspot.sh
@@ -1,5 +1,5 @@
 sysctl net.ipv4.ip_forward=1
 
-iptables -t nat -A POSTROUTING -o enp0s31f6 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o enp0s13f0u3u2 -j MASQUERADE
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlp0s20f3 -o enp0s31f6 -j ACCEPT
+iptables -A FORWARD -i wlp0s20f3 -o enp0s13f0u3u2 -j ACCEPT
