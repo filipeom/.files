233e039e88a26f27fe41a301e7d60b1c5bd1a694
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-07-06 22:46:00 +0100

    Update hyprland
    - Update borders and turn blur on
    - Use hypridle instead of swayidle
    - Add mod+X to lock screen with hyprlock

--- .config/hypr/scripts/bootstrap.sh
+++ .config/hypr/scripts/bootstrap.sh
@@ -2,14 +2,9 @@
 
 gammastep -c ~/.config/gammastep/gammastep.conf &
 dunst &
-hyprpaper &
-swayidle -w timeout 300 'swaylock -f -c 000000' \
-            timeout 600 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            timeout 900 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
-
-nextcloud &
-thunderbird &
-nm-applet &
 waybar &
+nm-applet &
+nextcloud &
+ferdium &
+firefox &
+thunderbird &
