3d901de035adc70dda5b398aa193e0fa63da6096
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-09-21 11:07:33 +0100

    Fix rust_analyzer loading

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,14 +1,15 @@
 local lsp = require('lsp-zero').preset({})
 
-lsp.on_attach(function(_, bufnr)
+lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
   -- lsp.default_keymaps(opts)
+
   vim.keymap.set('n', 'T', function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
   vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set('n', '<F3>', function() vim.lsp.buf.format() end, opts)
-  vim.keymap.set('n', '<leader>ca', function () vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
@@ -18,7 +19,8 @@ lsp.on_attach(function(_, bufnr)
     fg = "#999999",
     italic = true,
   })
-  vim.keymap.set('n', '<leader>i', function ()
+
+  vim.keymap.set('n', '<leader>i', function()
     vim.lsp.inlay_hint.enable(
       not vim.lsp.inlay_hint.is_enabled(opts),
       opts
@@ -26,6 +28,7 @@ lsp.on_attach(function(_, bufnr)
   end, opts)
 end)
 
+-- Servers
 lsp.setup_servers(
   { 'ts_ls'
   , 'eslint'
@@ -36,19 +39,7 @@ lsp.setup_servers(
   , 'pyright'
   , 'texlab'
   , 'lua_ls'
-})
-lsp.setup()
+  , 'rust_analyzer'
+  })
 
---  Configure rust-analyzer
-require('lspconfig').rust_analyzer.setup {
-  settings = {
-    ['rust-analyzer'] = {
-      check = {
-        extraArgs = {
-          "--target-dir",
-          "target/rust-analyzer"
-        }
-      }
-    }
-  }
-}
+lsp.setup()
